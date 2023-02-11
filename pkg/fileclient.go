7199184e1c9d8a7147698cea52a80ac1ca4db8fb
RuteSantos <1160663@isep.ipp.pt>
2023-02-11 19:40:03 +0000

    refactor FileClient to use LevelLogger

--- pkg/fileclient.go
+++ pkg/fileclient.go
@@ -1,26 +1,21 @@
 package pkg
 
 import (
-	"log"
 	"os"
 )
 
 type FileClient struct {
-	loggers []*log.Logger
+	loggers LevelLogger
 }
 
-func NewFileClient(fp string) (FileClient, error) {
+func NewFileClient(fp string, levels ...Level) (FileClient, error) {
 	file, err := os.OpenFile(fp, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0770)
 
 	if err != nil {
 		return FileClient{}, err
 	}
 
-	loggers := make([]*log.Logger, len(levelPrefix))
-
-	for i, v := range levelPrefix {
-		loggers[i] = log.New(file, v, defaultLoggerFlag)
-	}
+	loggers := NewLevelLogger(file, defaultLoggerFlag, levels)
 
 	return FileClient{
 		loggers: loggers,
