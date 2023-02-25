669dc67aa0f9ce0e933ab01c05d813b27ec71548
RuteSantos <1160663@isep.ipp.pt>
2023-02-25 16:52:41 +0000

    Use short file name and method name

--- pkg/pattern.go
+++ pkg/pattern.go
@@ -9,25 +9,25 @@ import (
 )
 
 const (
-	datePattern     = "%d%" // the date in the local time zone: 2009/01/23
-	fileNamePattern = "%f%" // full file name: /a/b/c/d.go
-	levelPattern    = "%L%" // Level: TRACE
-	linePattern     = "%l%" // line number: 23
-	methodPattern   = "%M%" // method name: main
-	messagePattern  = "%m%" // message to be logged: logging message
-	timePattern     = "%t%" // the time in the local time zone: 01:23:23
+	DatePattern     = "%d%" // the date in the local time zone: 2009/01/23
+	FileNamePattern = "%f%" // full file name: /a/b/c/d.go
+	LevelPattern    = "%L%" // Level: TRACE
+	LinePattern     = "%l%" // line number: 23
+	MethodPattern   = "%M%" // method name: main
+	MessagePattern  = "%m%" // message to be logged: logging message
+	TimePattern     = "%t%" // the time in the local time zone: 01:23:23
 )
 
-const callDepth = 4
+const callDepth = 5
 
-var Pattern = map[string]replaceFunction{
-	datePattern:     applyDatePattern(),
-	fileNamePattern: applyFileNamePattern(),
-	levelPattern:    applyLevelPattern(),
-	linePattern:     applyLinePattern(),
-	methodPattern:   applyMethodPattern(),
-	messagePattern:  applyMessagePattern(),
-	timePattern:     applyTimePattern(),
+var pattern = map[string]replaceFunction{
+	DatePattern:     applyDatePattern(),
+	FileNamePattern: applyFileNamePattern(),
+	LevelPattern:    applyLevelPattern(),
+	LinePattern:     applyLinePattern(),
+	MethodPattern:   applyMethodPattern(),
+	MessagePattern:  applyMessagePattern(),
+	TimePattern:     applyTimePattern(),
 }
 
 type replaceFunction func(format string, r Record) string
@@ -35,7 +35,7 @@ type replaceFunction func(format string, r Record) string
 func applyDatePattern() replaceFunction {
 	return func(format string, r Record) string {
 		year, month, day := time.Now().Date()
-		return strings.Replace(format, datePattern, fmt.Sprintf("%04d/%02d/%02d", year, month, day), -1)
+		return strings.Replace(format, DatePattern, fmt.Sprintf("%04d/%02d/%02d", year, month, day), -1)
 	}
 }
 
@@ -45,16 +45,16 @@ func applyFileNamePattern() replaceFunction {
 		_, file, _, ok := runtime.Caller(callDepth)
 
 		if ok {
-			replaceWith = file
+			replaceWith = getLastStringOfSeparator(file, '/', 2)
 		}
 
-		return strings.Replace(format, fileNamePattern, replaceWith, -1)
+		return strings.Replace(format, FileNamePattern, replaceWith, -1)
 	}
 }
 
 func applyLevelPattern() replaceFunction {
 	return func(format string, r Record) string {
-		return strings.Replace(format, levelPattern, r.Level.String(), -1)
+		return strings.Replace(format, LevelPattern, r.Level.String(), -1)
 	}
 }
 
@@ -67,7 +67,7 @@ func applyLinePattern() replaceFunction {
 			replaceWith = strconv.Itoa(no)
 		}
 
-		return strings.Replace(format, linePattern, replaceWith, -1)
+		return strings.Replace(format, LinePattern, replaceWith, -1)
 	}
 }
 
@@ -78,22 +78,41 @@ func applyMethodPattern() replaceFunction {
 		details := runtime.FuncForPC(pc)
 
 		if ok && details != nil {
-			replaceWith = details.Name()
+			name := details.Name()
+			replaceWith = getLastStringOfSeparator(name, '.', 1)
 		}
 
-		return strings.Replace(format, methodPattern, replaceWith, -1)
+		return strings.Replace(format, MethodPattern, replaceWith, -1)
 	}
 }
 
 func applyMessagePattern() replaceFunction {
 	return func(format string, r Record) string {
-		return strings.Replace(format, messagePattern, r.Message, -1)
+		return strings.Replace(format, MessagePattern, r.Message, -1)
 	}
 }
 
 func applyTimePattern() replaceFunction {
 	return func(format string, r Record) string {
 		hour, min, sec := time.Now().Clock()
-		return strings.Replace(format, timePattern, fmt.Sprintf("%02d:%02d:%02d", hour, min, sec), -1)
+		return strings.Replace(format, TimePattern, fmt.Sprintf("%02d:%02d:%02d", hour, min, sec), -1)
 	}
+}
+
+func getLastStringOfSeparator(value string, separator byte, count int) string {
+	short := value
+	c := 0
+
+	for i := len(value) - 1; i > 0; i-- {
+		if value[i] == separator {
+			c++
+
+			if c == count {
+				short = value[i+1:]
+				break
+			}
+		}
+	}
+
+	return short
 }
