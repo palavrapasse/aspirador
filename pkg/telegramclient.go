30bd910bfef839bbbdb785bf39d3c1702759b8c5
RuteSantos <1160663@isep.ipp.pt>
2023-02-25 16:55:46 +0000

    add PatternLayout to TelegramClient

--- pkg/telegramclient.go
+++ pkg/telegramclient.go
@@ -3,7 +3,8 @@ package pkg
 const messageType = "application/json"
 
 type TelegramClient struct {
-	loggers LevelLogger
+	loggers       LevelLogger
+	patternLayout PatternLayout
 }
 
 func NewTelegramClient(botToken, chatId string, levels ...Level) TelegramClient {
@@ -15,12 +16,19 @@ func NewTelegramClient(botToken, chatId string, levels ...Level) TelegramClient 
 	loggers := NewLevelLogger(tw, defaultLoggerFlag, levels)
 
 	return TelegramClient{
-		loggers: loggers,
+		loggers:       loggers,
+		patternLayout: defaultPatternLayout,
 	}
 }
 
+func (tc *TelegramClient) SetPatternLayout(p PatternLayout) {
+	tc.patternLayout = p
+}
+
 func (tc TelegramClient) Write(ar Record) {
-	tc.loggers[ar.Level].Println(ar.Message)
+	message := tc.patternLayout.FormatRecord(ar)
+
+	tc.loggers[ar.Level].Println(message)
 }
 
 func (tc TelegramClient) SupportsLevel(l Level) bool {
